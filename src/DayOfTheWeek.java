b4122fe5955d4c8dc17ccb3d4e83b3ff04108e24
durmuserdogan <drmserdogan@outlook.com>
2022-07-19 15:43:16 +0300

    second

--- src/DayOfTheWeek.java
+++ src/DayOfTheWeek.java
@@ -10,13 +10,13 @@ public class DayOfTheWeek {
                 System.out.println("Monday");
                 break;
             case 2:
-                System.out.println("Thursday");
+                System.out.println("Tuesday");
                 break;
             case 3:
                 System.out.println("Wednesday");
                 break;
             case 4:
-                System.out.println("Tuesday");
+                System.out.println("Thursday");
                 break;
             case 5:
                 System.out.println("Friday");
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         DayOfTheWeek.printDayOfTheWeek(4);
+        DayOfTheWeek.printN
     }
 }
