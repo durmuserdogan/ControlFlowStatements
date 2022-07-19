9bacd766b1e6443ba257d109efc845c3cb0ddf50
durmuserdogan <drmserdogan@outlook.com>
2022-07-19 16:41:09 +0300

    Number of days in month

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        DayOfTheWeek.printDayOfTheWeek(4);
-        NumberInWord.printNumberInWord(6);
+       // DayOfTheWeek.printDayOfTheWeek(4);
+       // NumberInWord.printNumberInWord(6);
+       // boolean t = NumberOfDaysInMonth.isLeapYear(2000);
+       // System.out.println(t);
+        int i = NumberOfDaysInMonth.getDaysInMonth(1,-2020);
+        System.out.println(i);
     }
+
+
 }
