9d0cbb663658e3e1ba67be797bc36dc58f0a280b
durmuserdogan <drmserdogan@outlook.com>
2022-07-20 15:49:40 +0300

    Sum Odd Challenge

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,25 @@ public class Main {
        // NumberInWord.printNumberInWord(6);
        // boolean t = NumberOfDaysInMonth.isLeapYear(2000);
        // System.out.println(t);
-        int i = NumberOfDaysInMonth.getDaysInMonth(1,-2020);
-        System.out.println(i);
+        //int i = NumberOfDaysInMonth.getDaysInMonth(1,-2020);
+        //System.out.println(i);
+
+/*        // Sum 3 and 5 Challenge
+        int sum=0;
+        int count=0;
+        for (int i=1; i<=1000;i++){
+            if ((i%3==0)&&(i%5==0)){
+                count ++;
+                sum+=i;
+                System.out.println("number founded"+i);
+            }
+            if (count== 5){
+                break;
+            }
+        }
+        System.out.println(sum);
+*/
+
+        System.out.println(SumOdd.sumOdd(100,1000));
     }
-
-
 }
