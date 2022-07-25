b067c4c8cef60685f98d1f37bb6092e91a8fe688
durmuserdogan <drmserdogan@outlook.com>
2022-07-25 15:18:19 +0300

    All Factors..

--- src/AllFactors.java
+++ src/AllFactors.java
@@ -7,7 +7,7 @@ public class AllFactors {
         else{
             for(int i=1; i<=number; i++){
                 if(number%i==0){
-                    System.out.print(i);
+                    System.out.print(i+ " ");
                 }
 
             }
