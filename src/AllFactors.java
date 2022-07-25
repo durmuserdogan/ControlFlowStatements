878dc3eadbca76005787055509c0f8efc7de93a6
durmuserdogan <drmserdogan@outlook.com>
2022-07-25 15:16:10 +0300

    All Factors_

--- src/AllFactors.java
+++ src/AllFactors.java
@@ -1,11 +1,11 @@
 public class AllFactors {
-    public static int printFactors(int number){
-        int i=1;
+    public static void printFactors(int number){
+
         if (number<0){
             System.out.println("Invalid Value");
         }
         else{
-            for(; i<=number; i++){
+            for(int i=1; i<=number; i++){
                 if(number%i==0){
                     System.out.print(i);
                 }
@@ -14,6 +14,6 @@ public class AllFactors {
 
 
         }
-        return i;
+
     }
 }
