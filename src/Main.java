0a04408293d541f9295fe4b155442889301871fe
durmuserdogan <drmserdogan@outlook.com>
2022-07-28 13:54:25 +0300

    Reading User Input

--- src/Main.java
+++ src/Main.java
@@ -73,8 +73,25 @@ public class Main {
         int i=LargestPrime.getLargestPrime(217);
         System.out.println(i);*/
 
-        // Diagonal Star
-        DiagonalStar.printSquareStar(24);
+/*        // Diagonal Star
+        DiagonalStar.printSquareStar(24);*/
+
+/*        //Reading User Input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth:");
+        int yearOfBirth = scanner.nextInt();
+        scanner.nextLine(); // handle next line character (enter key)
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        int age = 2022 - yearOfBirth;
+
+        System.out.println("Your name is " + name + ", and you are "+ age+ " years old. ");
+        scanner.close();*/
+
+
+
 
 
 
